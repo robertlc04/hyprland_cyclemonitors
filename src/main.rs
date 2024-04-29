ef824dcdbc84ac0acfd6c3c54eaa260b318297e4
Robert <robertlc04@github.com>
2024-04-29 18:32:45 -0400

    Rewrite using the IPC of hyprland

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,40 @@
-use hyprland::data::*;
-use hyprland::prelude::*;
-use hyprland::dispatch::*;
-use hyprland::Result;
+use std::env;
+use std::io::{Read, Write};
+use std::os::unix::net::UnixStream;
+use json;
 
-struct MyMon {
-    name: String,
-    focused: bool,
-}
+fn main() -> std::io::Result<()> {
+    let his: String = env::var("HYPRLAND_INSTANCE_SIGNATURE").unwrap_or("none".to_string());
+    if his.contains("none") {
+        return Err(std::io::Error::new(std::io::ErrorKind::NotFound, "Not found"))
+    }
 
-fn main() -> Result<()> {
+    let shypr_path = format!("/tmp/hypr/{his}/.socket.sock");
+    let mut value = String::new();
+    let mut stream = UnixStream::connect(shypr_path.clone())?;
+    stream.write(b"-j/monitors")?;
 
 
-    let monitors = list_mons()?;
+    stream.read_to_string(&mut value)?;
+    stream.shutdown(std::net::Shutdown::Both)?;
 
-    for mon in monitors {
-        if !mon.focused {
-            Dispatch::call(DispatchType::FocusMonitor(MonitorIdentifier::Name(&mon.name)) )?;
+    let obj = json::parse(&value.to_string()).unwrap();
+
+    for i in 0..obj.len() {
+        match obj[i]["focused"].as_bool() {
+            Some(v) => {
+                if !v {
+                    value = obj[i]["name"].to_string()
+                }
+            },
+            None => {}
         }
     }
 
+    let value = format!("/dispatch focusmonitor {}",value);
+    let mut stream = UnixStream::connect(shypr_path.clone())?;
+    stream.write(value.as_bytes())?;
+    stream.shutdown(std::net::Shutdown::Both)?;
 
     Ok(())
-}
-
-fn list_mons() -> Result<Vec<MyMon>> {
-    let monitors = Monitors::get()?.to_vec();
-
-    let mut mons_name = Vec::new();
-
-    for monitor in monitors {
-        mons_name.push(MyMon {
-            name: monitor.name.clone(),
-            focused: monitor.focused
-        });
-    }
-
-    return Ok(mons_name);
 }
